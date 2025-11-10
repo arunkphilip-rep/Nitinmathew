f4431ee0a2573e2214b8641641bfbbac552c33e0
arunkphilip-rep <arun.csa2125@saintgits.org>
2025-11-10 19:42:04 +0530

    publish

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Nitinmathew/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
